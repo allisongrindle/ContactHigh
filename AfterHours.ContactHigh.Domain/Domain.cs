a85adc6dc9af2951bbb8c93f1e597098a3271859
unknown <Allison.Grindle@AGRINDLE7DAL.pariveda.local>
2015-05-13 18:31:49 -0500

    Domain design

--- AfterHours.ContactHigh.Domain/Domain.cs
+++ AfterHours.ContactHigh.Domain/Domain.cs
@@ -6,6 +6,16 @@ using System.Threading.Tasks;
 
 namespace AfterHours.ContactHigh.Domain
 {
+
+    public class Junk
+    {
+        public void doStuff()
+        {
+            var Address = new Address("123 Main", new Location("Dallas", "TX"), Domain.Address.AddressType.Home);
+        }
+    }
+
+
     public interface IAppService
     {
         Person GetPerson(int id);
@@ -27,23 +37,114 @@ namespace AfterHours.ContactHigh.Domain
         Confirmation DelayReminder(TimeSpan delayAmount);
     }
 
+    public class PersonService
+    {
+        public Confirmation AddPerson(Person person) {
+
+            // create reminders
+            
+
+            return null;
+        }
+
+
+    }
+
+    public class InteractionService
+    {
+
+        public Confirmation ChangeInteractionDate(Interaction interaction, DateTime date)
+        {
+
+
+            return null;
+        }
+    }
+
     public class Person
     {
-        public int Id { get; set; }
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
+        public Person(string firstName, string lastName)
+        {
+            Id = new Guid();
+            FirstName = firstName;
+            LastName = lastName;
+            LastInteractionDate = DateTime.Today;
+        }
+
+        public Guid Id { get; private set; }
+        public string FirstName { get; private set; }
+        public string LastName { get; private set; }
         public List<string> PhoneNumber { get; set; }
         public List<string> Email { get; set; }
         public List<Address> Address { get; set; }
         public DateTime Birthday { get; }
         public ContactFrequency Frequency { get; set; }
+        public DateTime LastInteractionDate { get; set; }
+
+        public class Address
+        {
+
+            private Location _location;
+
+            public Address(string line1, Location location, AddressType type)
+            {
+                Line1 = line1;
+                _location = location;
+                Type = type;
+            }
+
+            public enum AddressType
+            {
+                Home, 
+                Work,
+                Other
+            }
+
+            public string Line1 { get; private set; }
+            public string Line2 { get; set; }
+            public string Line3 { get; set; }
+            public string City 
+            {
+                get 
+                { 
+                    return _location.City; 
+                }
+            }
+            public string State
+            {
+                get
+                {
+                    return _location.State;
+                }
+            }
+            public string Zip { get; set; }
+            public AddressType Type { get; set; }
+        }
     }
 
-    public class Address
-    { }
+
+    public class Location
+    {
+        public Location(string city, string state)
+        {
+            City = city;
+            State = state;
+        }
+
+        public string City { get; private set; }
+        public string State { get; private set; }
+    }
 
     public class Interaction
-    { }
+    {
+        public Interaction(Guid personId, DateTime date, string interactionType)
+        {
+            Date = date;
+        }
+
+        public DateTime Date { get; set; }
+        public string InteractionType { get; private set; }
+    }
 
     public class ContactFrequency
     { }
@@ -65,3 +166,4 @@ namespace AfterHours.ContactHigh.Domain
     public class Confirmation
     { }
 }
+    
